38a66b54e63e54e1c2b608d2f736b0555ca6ae23
Elbert Kim/LGEUS Americas Information Center(elbert.kim@lgepartner.com) <elbert.kim@lge.com>
2018-11-16 15:33:05 -0500

    let's go live

--- server.js
+++ server.js
@@ -29,4 +29,48 @@ app.get('/dates.json', (req, res) => {
 	});
 });
 
+app.post('/dates.json', (req, res) => {
+	fs.readFile("dates.json", (err, data) => {
+		if (err) throw err;
+		let json = JSON.parse(data);
+		let reqData = req.body;
+		reqData['allDay'] = false;
+		console.log(reqData);
+		json.push(reqData);
+		console.log(json);
+
+		// add to dates.json
+		fs.writeFile("dates.json", JSON.stringify(json, null, 4));
+		res.sendStatus(200);
+	});
+});
+
+app.delete('/dates.json', (req, res) => {
+
+	fs.readFile("dates.json", (err, data) => {
+		if (err) throw err;
+
+		// array
+		let json = JSON.parse(data);
+
+		// event.title
+		let listOfPropertyNames = Object.getOwnPropertyNames(req.body);
+
+		let datesArray = [];
+		for(var i = 0; i < json.length; i++){
+
+			if(json[i]['title'] == listOfPropertyNames){
+				continue;
+			} 
+			
+			datesArray.push(json[i]);
+		};
+
+		fs.writeFile("dates.json", JSON.stringify(datesArray, null, 4));
+	});
+
+	res.sendStatus(200);
+
+});
+
 app.listen(port, () => console.log("Listening at port " + port));
